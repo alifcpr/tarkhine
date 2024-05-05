140b397d1694b7bdbd378d45890e589a9afa766f
test <esfahani.aliyahoo@gmail.com>
2024-05-05 12:42:47 +0330

    build: payment verify page

--- app/(root)/v1/payment/verify/page.tsx
+++ app/(root)/v1/payment/verify/page.tsx
@@ -1,12 +1,50 @@
+"use client";
 import ErrorPayment from "@/components/payment/ErrorPayment";
 import SuccessPayment from "@/components/payment/SuccessPayment";
-import React from "react";
+import React, { useLayoutEffect, useState } from "react";
+
+interface PaymentInfo {
+  status: number;
+  trackId: number;
+  success: number;
+}
 
 const Page = () => {
+  // initial state
+  const [initial, setInitial] = useState(false);
+  // query state
+  const [paymentInfo, setPaymentInfo] = useState<PaymentInfo>({
+    status: 0,
+    trackId: 0,
+    success: 0,
+  });
+
+  // remove all queries 
+  useLayoutEffect(() => {
+    setTimeout(() => {
+      window.history.replaceState(null, "", window.location.pathname);
+    }, 10);
+
+    const Url = new URL(window.location.href);
+    setPaymentInfo({
+      status: Number(Url.searchParams.get("status")) ?? 0,
+      success: Number(Url.searchParams.get("success")) ?? 0,
+      trackId: Number(Url.searchParams.get("trackId")) ?? 0,
+    });
+
+    setTimeout(() => {
+      setInitial(true);
+    }, 200);
+  }, []);
+
   return (
     <div className="h-[600px]">
-      {/* <SuccessPayment trackId={93482348} /> */}
-      {/* <ErrorPayment trackId={2398324} /> */}
+      {initial &&
+        (paymentInfo.success === 1 ? (
+          <SuccessPayment trackId={paymentInfo.trackId} />
+        ) : (
+          <ErrorPayment trackId={paymentInfo.trackId} />
+        ))}
     </div>
   );
 };
